85004e8d96bbba4106da101b38ca44e4d7dad8d6
pralphv <chowzhiho321@gmail.com>
2020-03-20 01:07:51 +0800

    first

---
+++ src/data/business_days/api.py
@@ -0,0 +1,37 @@
+from typing import List
+import logging
+import os
+import requests
+
+from cachetools import cached
+from dotenv import load_dotenv
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s:%(levelname)s: %(message)s")
+
+CACHE = {}
+
+
+@cached(CACHE)
+def fetch_data() -> List:
+    """
+    Fetches list of business days from firebase.
+    :return:
+    """
+    url = os.environ.get('FIREBASE_DATABASE_URL')
+    url = f'{url}/businessDays/data.json'
+    logging.info(f'Fetching business days from {url}')
+    resp = requests.get(url).json()
+    logging.info(f'Successfully business days fetched {url}')
+    return resp
+
+
+def clear_cache():
+    CACHE.clear()
+    logging.debug(f'Cleared business days cache')
+
+
+if __name__ == '__main__':
+    load_dotenv()
+
+    data = fetch_data()
+    data = fetch_data()  # logs should only appear once
