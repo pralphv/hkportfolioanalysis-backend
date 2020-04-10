983d6f5be0e3f29bd71d9d82836e9b4bfb6d40a3
pralphv <chowzhiho321@gmail.com>
2020-04-10 10:03:24 +0800

    changed: exception logging: warning to error

--- src/app.py
+++ src/app.py
@@ -124,7 +124,7 @@ async def run_hkportfolioanalysis_bundle(parameters: models.Bundle):
         }
         return bundle
     except Exception as e:
-        logging.warning(f'Failed request:\n{e}\n{parameters}')
+        logging.error(f'Failed request:\n{e}\n{parameters}')
         return 'Bad Request'
 
 
